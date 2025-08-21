c3da5568d071d9aa716aae5c4d396f1076f83427
h1divp <71522316+h1divp@users.noreply.github.com>
2025-08-21 01:09:46 -0400

    Created basic styling for mobile sign

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	<!-- Ground with curved top -->
 	<div class="absolute bottom-0 hidden h-full w-screen md:flex">
 		<svg class="absolute bottom-0 h-[40%] w-full" viewBox="0 0 100 30" preserveAspectRatio="none">
-			<path d="M0,10 Q50,0 100,10 L100,30 L0,30 Z" fill="#6EC21D" />
+			<path d="M0,10 Q50,0 100,10 L100,30 L0,30 Z" fill="#B4CF9B" />
 		</svg>
 
 		<!-- ROAD -->
@@ -14,32 +14,41 @@
 	<!-- Mobile grass -->
 	<div class="absolute bottom-0 h-1/3 w-screen bg-grass"></div>
 
-	<!-- Mobile Sign -->
+	<!-- Mobile Signs -->
 	<div
-		class="absolute top-1/2 left-1/2 flex min-h-[30vh] w-[80vw] -translate-x-1/2 -translate-y-5/6 transform flex-col rounded-xl border-6 border-white bg-interstate-green"
+		class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-5/6"
 	>
-		<!-- Inner container-->
-		<div class="relative flex w-full flex-1 flex-col justify-center gap-8 px-8">
-			<div class="flex flex-row items-center gap-6">
-				<img
-					class="h-16"
-					src="/interstate_logo_sh.svg"
-					alt="Interstate logo with SwampHacks Logo"
-				/>
-				<h2 class="text-center text-white">University of Florida's Flagship Hackathon</h2>
+		<div class="flex flex-col rounded-xl min-h-[30vh] w-[80vw] justify-center items-center border-6 border-white bg-interstate-green">
+			<!-- Inner container-->
+			<div class="flex flex-col justify-center gap-8 px-12">
+				<div class="flex flex-row items-center gap-2">
+					<img
+						class="h-16"
+						src="/interstate_logo_sh.svg"
+						alt="Interstate logo with SwampHacks Logo"
+					/>
+					<h2 class="text-center text-white font-medium">University of Florida's Flagship Hackathon</h2>
+				</div>
+				<h1 class="text-3xl tracking-wide text-white text-center font-extrabold">SWAMPHACKS XI</h1>
+				<h2 class="text-xl text-white text-center font-medium">160 DAYS</h2>
 			</div>
-			<h1 class="text-3xl font-semibold tracking-wide text-white">SwampHacks XI</h1>
 		</div>
+		<!-- Signposts -->
+		<div class="flex flex-col rounded-xl min-h-[6vh] w-[80vw] justify-center mt-3 border-6 border-white bg-interstate-green">
+			<p class="text-xl text-white text-center font-extrabold">JOIN MAILING LIST</p>
 
-		<!-- Exit only sign-->
-		<!-- Exit only sign -->
-		<div
-			class="absolute bottom-0 h-12 w-full rounded-b-md bg-yellow-300 flex flex-row justify-between items-center"	
-		></div>
-		<!-- Pole 1 -->
-		<div></div>
-
-		<!-- Pole 2 -->
-		<div></div>
+		</div>
+		<div class="block min-w-max fill-signpost-gray">
+			<div class="absolute left-1/5 -translate-y-1/5 -z-1">
+				<svg class="min-h-screen" width="15" xmlns="http://www.w3.org/2000/svg">
+					<rect width="15" height="1000"></rect>
+				</svg>
+			</div>
+			<div class="absolute right-1/5 -translate-y-1/5 -z-1">
+				<svg class="min-h-screen" width="15" xmlns="http://www.w3.org/2000/svg">
+					<rect width="15" height="1000"></rect>
+				</svg>
+			</div>
+		</div>
 	</div>
 </main>
