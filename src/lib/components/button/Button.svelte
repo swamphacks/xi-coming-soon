e02745cb0e18a5ff367f8760cdeb1907f732f7a5
h1divp <71522316+h1divp@users.noreply.github.com>
2025-08-27 00:51:42 -0400

    improvement: styling and breakpoints

--- src/lib/components/button/Button.svelte
+++ src/lib/components/button/Button.svelte
@@ -1,5 +1,5 @@
 <button
-	class={`${$$props.class || ''} cursor-pointer rounded border-b-4 border-button-border bg-button px-4 py-2 font-bold text-button-text hover:bg-button-hover active:border-button-active active:bg-button-active active:text-button-active-text`}
+	class={`${$$props.class || ''} cursor-pointer rounded border-2 border-b-6 border-button-border bg-button px-4 py-2 font-bold text-button-text hover:bg-button-hover active:border-button-active active:bg-button-active active:text-button-active-text`}
 	on:click
 >
 	<slot />
